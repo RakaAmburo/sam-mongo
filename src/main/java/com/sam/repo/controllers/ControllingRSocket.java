228a12a16ec10ed31e573498a0b2dc5b34a63937
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2020-11-07 09:31:08 +0100

    preparing sam mongo

---
+++ src/main/java/com/sam/repo/controllers/ControllingRSocket.java
@@ -0,0 +1,41 @@
+package com.sam.repo.controllers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+@Controller
+@Log4j2
+public class ControllingRSocket {
+
+    @MessageMapping("startPing")
+    Flux<String> startPing() {
+
+
+        Flux<String> pingSignal =
+                Flux.fromStream(Stream.generate(() -> "ping")).delayElements(Duration.ofMillis(1000));
+
+
+        return pingSignal;
+    }
+
+    @MessageMapping("channel")
+    Flux<BigRequest> channel(RSocketRequester clientRSocketConnection, Flux<BigRequest> bigRequestFlux) {
+
+        return Flux.create(
+                (FluxSink<BigRequest> sink) -> {
+                    bigRequestFlux
+                            .doOnNext(
+                                    i -> {
+                                        sink.next(i);
+                                    })
+                            .subscribe();
+                });
+    }
+}
