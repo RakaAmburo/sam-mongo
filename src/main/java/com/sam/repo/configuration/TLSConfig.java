d86a8450c58baa980246c91ee577076a1084c872
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2021-02-27 08:37:41 +0100

    fxing

---
+++ src/main/java/com/sam/repo/configuration/TLSConfig.java
@@ -0,0 +1,31 @@
+package com.sam.repo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class TLSConfig {
+
+  @Value("${mongo.client.trustStore}")
+  private String trustStore;
+
+  @Value("${mongo.client.trustStorePassword}")
+  private String trustStorePassword;
+
+  @Value("${mongo.client.keyStore}")
+  private String keyStore;
+
+  @Value("${mongo.client.keyStorePassword}")
+  private String keyStorePassword;
+
+  @PostConstruct
+  private void configureTLS() {
+    System.setProperty("javax.net.ssl.trustStore", trustStore);
+    System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+    System.setProperty("javax.net.ssl.keyStore", keyStore);
+    System.setProperty("javax.net.ssl.keyStorePassword", keyStorePassword);
+    System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
+  }
+}
